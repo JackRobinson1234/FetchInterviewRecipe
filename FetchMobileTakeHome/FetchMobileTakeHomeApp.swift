27a7db014d03360fdb9a7a63120e9fa045f67d39
JackRobinson1234 <Jtrobinsonhb@gmail.com>
2024-11-21 08:18:54 -0800

    Initial Commit

---
+++ FetchMobileTakeHome/FetchMobileTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMobileTakeHomeApp.swift
+//  FetchMobileTakeHome
+//
+//  Created by Jack Robinson on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchMobileTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
